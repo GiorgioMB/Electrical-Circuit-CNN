158679d6efd837fd7014fc2ed4ebec42bd7d755e
Giorgio <122452088+GiorgioMB@users.noreply.github.com>
2024-04-13 15:25:35 +0200

    Update model.py

--- model.py
+++ model.py
@@ -1,17 +1,50 @@
 #%%
-import torchvision as tv
-import torch as t
-import os 
-import pandas as pd
+import os
 import torch
-from torch.utils.data import Dataset, DataLoader
-import torchvision.transforms as transforms
+import torchvision
+from torchvision.models.detection import FasterRCNN
+from torchvision.models.detection.rpn import AnchorGenerator
+from torchvision.transforms.functional import to_tensor
 import torchvision.models as models
-import torch.optim as optim
 import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+from PIL import Image
+import pandas as pd
 import ssl
-from PIL import Image 
+
+# Disabling SSL verification to allow image loading from web if needed
 ssl._create_default_https_context = ssl._create_unverified_context
+
+class CustomDataset(Dataset):
+    def __init__(self, images, boxes, transform=None):
+        self.images = images
+        self.boxes = boxes
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+        image_name = list(self.images.keys())[idx]
+        image = self.images[image_name]
+        box_data = self.boxes[image_name]
+
+        if self.transform:
+            image = self.transform(image)
+
+        boxes = torch.as_tensor(box_data, dtype=torch.float32)
+        labels = torch.ones((boxes.shape[0],), dtype=torch.int64)  # Assuming class label 1 for all
+
+        target = {'boxes': boxes, 'labels': labels}
+        return image, target
+
+def collate_fn(batch):
+    images, targets = zip(*batch)
+    images = torch.stack([to_tensor(img) for img in images], 0)
+    return images, targets
+
+
 classes = set()
 boxes = {}
 imgs = {}
@@ -35,39 +68,14 @@ for i in range(26):
             resized_image = image.resize((2048, 2048))
             key_to_save = file.split('.')[0]
             imgs[key_to_save] = image
+    
 num_classes = len(classes) + 1
 filtered = set(imgs.keys()).intersection(set(boxes.keys()))
 imgs_filter = {k: imgs[k] for k in filtered}
 boxes_filter = {k: boxes[k] for k in filtered}
-class CustomDataset(Dataset):
-    def __init__(self, images, boxes, transform=None):
-        self.images = images
-        self.boxes = boxes
-        self.transform = transform
 
-    def __len__(self):
-        return len(self.images)
-
-    def __getitem__(self, idx):
-        image_name = list(self.images.keys())[idx]
-        print("Retrieving image: ", image_name)
-        try:
-            image = self.images[image_name]
-            boxes = self.boxes[image_name]
-        except Exception as e:
-            print("Error: ", e, type(e))
-            raise e
-
-        if self.transform:
-            image = self.transform(image)
-        return image, boxes
-
-transform = transforms.Compose([
-    transforms.ToTensor(),
-])
-
-dataset = CustomDataset(images=imgs_filter, boxes=boxes_filter, transform=transform)
-data_loader = DataLoader(dataset, batch_size=4, shuffle=True)
+dataset = CustomDataset(imgs_filter, boxes_filter, transform=None)
+data_loader = DataLoader(dataset, batch_size=2, shuffle=True, collate_fn=collate_fn)
 
 model = models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
 in_features = model.roi_heads.box_predictor.cls_score.in_features
@@ -75,7 +83,7 @@ model.roi_heads.box_predictor = models.detection.faster_rcnn.FastRCNNPredictor(i
 
 criterion = nn.SmoothL1Loss()
 
-optimizer = optim.Adam(model.parameters(), lr=1e-5, amsgrad = True)
+optimizer = optim.Adagrad(model.parameters(), lr=0.001)
 num_epochs = 10
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model.to(device)
