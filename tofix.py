8a6c41fecf3ce9f5087c98f981badebb6b818403
Sofro21 <148988650+Sofro21@users.noreply.github.com>
2024-04-13 16:04:23 +0200

    Update tofix.py

--- tofix.py
+++ tofix.py
@@ -1,3 +1,17 @@
+dataset = CustomDataset(images=imgs_filter, boxes=boxes_filter, transform=transform)
+data_loader = DataLoader(dataset, batch_size=4, shuffle=True, collate_fn =collate_fn)
+
+model = models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+in_features = model.roi_heads.box_predictor.cls_score.in_features
+model.roi_heads.box_predictor = models.detection.faster_rcnn.FastRCNNPredictor(in_features, num_classes)
+
+criterion = nn.SmoothL1Loss()
+
+optimizer = optim.Adam(model.parameters(), lr=1e-5, amsgrad = True)
+num_epochs = 10
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model.to(device)
+model.train()
 for epoch in range(num_epochs):
     running_loss = 0.0
     for images, targets in data_loader:
