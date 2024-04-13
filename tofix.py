3d86d5cdf0074d644691da2459f2e6ca6034dd34
Sofro21 <148988650+Sofro21@users.noreply.github.com>
2024-04-13 16:02:21 +0200

    Update tofix.py

--- tofix.py
+++ tofix.py
@@ -1,17 +1,18 @@
 for epoch in range(num_epochs):
     running_loss = 0.0
     for images, targets in data_loader:
-        print(images)
-        images = images.to(device)
-        print('Loaded Images')
-        targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
-        print('Loaded y')
-        optimizer.zero_grad()
-        loss_dict = model(images, targets)
-        losses = sum(loss for loss in loss_dict.values())
-        losses.backward()
-        optimizer.step()
-        running_loss += losses.item()
+        for imgs, targ in zip(images, targets):
+            print(images)
+            imgs = imgs.to(device)
+            print('Loaded Images')
+            targ = targ.to(device)
+            print('Loaded y')
+            optimizer.zero_grad()
+            loss_dict = model(imgs, targ)
+            losses = sum(loss for loss in loss_dict.values())
+            losses.backward()
+            optimizer.step()
+            running_loss += losses.item()
     print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {running_loss/len(data_loader)}')
 
 torch.save(model.state_dict(), 'faster_rcnn_model.pth')
